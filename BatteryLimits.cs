198445eefdb5d3da711f526734b1163a5a1c7e0c
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-09 23:21:31 +0530

    Update BatteryLimits.cs

--- BatteryLimits.cs
+++ BatteryLimits.cs
@@ -4,36 +4,36 @@ using System.Text;
 
 public static class BatteryLimits 
 {
-    private static IReadOnlyDictionary<Interval<float>, string> _stateOfChargeBoundary
-    = new Dictionary<Interval<float>, string>
+    private static IReadOnlyDictionary<Interval, Tuple<string,string>> _stateOfChargeBoundary
+    = new Dictionary<Interval, Tuple<string, string>>
        {
-           { new Interval<float>(0, 20), "LOW_SOC_BREACH" },
-           { new Interval<float>(21, 24), "LOW_SOC_WARNING" },
-           { new Interval<float>(25, 75), "NORMAL"},
-           { new Interval<float>(76, 80), "HIGH_SOC_WARNING" },
-           { new Interval<float>(81, 100), "HIGH_SOC_BREACH" }
+           { new Interval(0, 20), new Tuple<string,string>("The state of charge is low compared to the minimum limits","Der Ladezustand ist im Vergleich zu den Mindestgrenzen niedrig") },
+           { new Interval(21, 24), new Tuple<string,string>("Warning! State of charge is heading towards minimum limit value","Warnung! Der Ladezustand nähert sich dem Mindestgrenzwert") },
+           { new Interval(25, 75), new Tuple<string,string>("State of Charge is Normal","Der Ladezustand ist normal")},
+           { new Interval(76, 80), new Tuple<string,string>("Warning! State of charge is heading towards maximum limit value","Warnung! Der Ladezustand nähert sich dem maximalen Grenzwert") },
+           { new Interval(81, 100), new Tuple<string,string>("The state of charge is high compared to the maximum limits","Der Ladezustand ist im Vergleich zu den Höchstgrenzen hoch") }
         };
-    public static IReadOnlyDictionary<Interval<float>, string> stateOfChargeBoundary => _stateOfChargeBoundary;
+    public static IReadOnlyDictionary<Interval, Tuple<string, string>> stateOfChargeBoundary => _stateOfChargeBoundary;
 
-   private static IReadOnlyDictionary<Interval<float>, string> _temperatureBoundary
-   = new Dictionary<Interval<float>, string>
+   private static IReadOnlyDictionary<Interval, Tuple<string, string>> _temperatureBoundary
+   = new Dictionary<Interval, Tuple<string, string>>
       {
-           { new Interval<float>(-45, -2.26f), "LOW_TEMPERATURE_BREACH" },
-           { new Interval<float>(-2.25f, -1), "LOW_TEMPERATURE_WARNING" },
-           { new Interval<float>(0, 42.75f), "NORMAL"},
-           { new Interval<float>(42.76f, 45), "HIGH_TEMPERATURE_WARNING" },
-           { new Interval<float>(46, 100), "HIGH_TEMPERATURE_BREACH" }
+           { new Interval(-45, -2.26f), new Tuple<string,string>("The temperature is low compared to the minimum limits","Die Temperatur ist im Vergleich zu den Mindestgrenzwerten niedrig") },
+           { new Interval(-2.25f, -1), new Tuple<string,string>("Warning! Temperature is heading towards minimum limit value","Warnung! Die Temperatur nähert sich dem Mindestgrenzwert") },
+           { new Interval(0, 42.75f), new Tuple<string,string>("Temperature is Normal","Die Temperatur ist normal")},
+           { new Interval(42.76f, 45), new Tuple<string,string>("Warning! Temperature is heading towards maximum limit value","Warnung! Die Temperatur nähert sich dem maximalen Grenzwert") },
+           { new Interval(46, 100), new Tuple<string,string>("The temperature is high compared to the maximum limits","Die Temperatur ist im Vergleich zu den Höchstgrenzen hoch") }
        };
-    public static IReadOnlyDictionary<Interval<float>, string> temperatureBoundary => _temperatureBoundary;
+    public static IReadOnlyDictionary<Interval, Tuple<string, string>> temperatureBoundary => _temperatureBoundary;
 
-    private static IReadOnlyDictionary<Interval<float>, string> _chargeRateBoundary
-   = new Dictionary<Interval<float>, string>
+    private static IReadOnlyDictionary<Interval, Tuple<string, string>> _chargeRateBoundary
+   = new Dictionary<Interval, Tuple<string, string>>
       {
-           { new Interval<float>(0, 0.75f), "NORMAL"},
-           { new Interval<float>(0.76f, 0.8f), "HIGH_CHARGERATE_WARNING" },
-           { new Interval<float>(0.81f, 100), "HIGH_CHARGERATE_BREACH" }
+           { new Interval(0, 0.75f), new Tuple<string,string>("Charge Rate is Normal","Laderate ist normal")},
+           { new Interval(0.76f, 0.8f), new Tuple<string,string>("Warning! Charge rate is heading towards maximum limit value","Warnung! Der Gebührensatz bewegt sich in Richtung des maximalen Grenzwerts") },
+           { new Interval(0.81f, 100), new Tuple<string,string>("The charge rate is high compared to the maximum limits","Die Laderate ist im Vergleich zu den Höchstgrenzen hoch") }
        };
-    public static IReadOnlyDictionary<Interval<float>, string> chargeRateBoundary => _chargeRateBoundary;
+    public static IReadOnlyDictionary<Interval, Tuple<string, string>> chargeRateBoundary => _chargeRateBoundary;
 
 }
 
