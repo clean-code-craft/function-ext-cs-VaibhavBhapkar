c679ece1f0ddac862698b662a6cd232344828cad
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-09 21:55:27 +0530

    Create Interval.cs

---
+++ Interval.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Interval<T> where T : struct,IComparable
+{
+    public Nullable<T> Start { get; set; }
+    public Nullable<T> End { get; set; }
+
+    public Interval(T start, T end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public bool InRange(T value)
+    {
+        return ((!Start.HasValue || value.CompareTo(Start.Value) >= 0) &&
+                (!End.HasValue || End.Value.CompareTo(value) >= 0));
+    }
+}
