10c400a108cf3ab3d2425eb2024a81d1f8549d32
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-17 16:45:24 +0530

    Update BatteryExamine.cs

--- BatteryExamine.cs
+++ BatteryExamine.cs
@@ -5,67 +5,70 @@ using System.Text;
 
 public class BatteryExamine
 {
+
     string message = string.Empty;
-    public void BatteryIsOk(BatteryFactors batteryFactors)
+    Dictionary<string, string> batteryAlerts = new Dictionary<string, string>();
+    
+    public Dictionary<string, string> BatteryIsOk(BatteryFactors batteryFactors)
     {
-        CompareTemperatureWithRange(batteryFactors.batteryTemprature,batteryFactors.operatingLanguage);
-        CompareStateOfChargeWithRange(batteryFactors.batteryStateOfCharge,batteryFactors.operatingLanguage);
-        CompareChargeRateWithRange(batteryFactors.batteryChargeRate,batteryFactors.operatingLanguage);
+        batteryAlerts.Clear();
+        if(batteryFactors.temperatureMesureUnit== "Fahrenheit")
+        {
+            batteryFactors.batteryTemprature = BatteryFeatureConversion.ConvertTemperatureFahrenheitToCelsius(batteryFactors.batteryTemprature);
+        }
+        CompareTemperatureWithRange(batteryFactors.batteryTemprature, batteryFactors.operatingLanguage);
+        CompareStateOfChargeWithRange(batteryFactors.batteryStateOfCharge, batteryFactors.operatingLanguage);
+        CompareChargeRateWithRange(batteryFactors.batteryChargeRate, batteryFactors.operatingLanguage);
+        return batteryAlerts;
     }
-    private void CompareTemperatureWithRange(float batteryTemperature,string operatingLanguage)
+    private void CompareTemperatureWithRange(float batteryTemperature, string operatingLanguage)
     {
         foreach (var temperatureValue in BatteryLimits.temperatureBoundary)
         {
             if (temperatureValue.Key.InRange(batteryTemperature))
             {
-                if (operatingLanguage == "English")
-                {
-                    DisplayMessage(temperatureValue.Value.Item1);
-                }
-                else
-                {
-                    DisplayMessage(temperatureValue.Value.Item2);
-                }
+                AccumulateBreachedFeatures(temperatureValue.Value, operatingLanguage, "Temperature");
             }
         }
     }
-    public void CompareStateOfChargeWithRange(float stateOfCharge,string operatingLanguage)
+    public void CompareStateOfChargeWithRange(float stateOfCharge, string operatingLanguage)
     {
         foreach (var socValue in BatteryLimits.stateOfChargeBoundary)
         {
             if (socValue.Key.InRange(stateOfCharge))
             {
-                if(operatingLanguage=="English")
-                {
-                    DisplayMessage(socValue.Value.Item1);
-                }
-                else
-                {
-                    DisplayMessage(socValue.Value.Item2);
-                }
+                AccumulateBreachedFeatures(socValue.Value, operatingLanguage, "State Of Charge");
             }
         }
     }
-    public void CompareChargeRateWithRange(float chargeRate,string operatingLanguage)
+    public void CompareChargeRateWithRange(float chargeRate, string operatingLanguage)
     {
         foreach (var chargeRateValue in BatteryLimits.chargeRateBoundary)
         {
             if (chargeRateValue.Key.InRange(chargeRate))
             {
-                if (operatingLanguage == "English")
-                {
-                    DisplayMessage(chargeRateValue.Value.Item1);
-                }
-                else
-                {
-                    DisplayMessage(chargeRateValue.Value.Item2);
-                }
+                AccumulateBreachedFeatures(chargeRateValue.Value, operatingLanguage, "Charge Rate");
             }
         }
     }
-    static void DisplayMessage(string displayMessage)
+    public void AccumulateBreachedFeatures(Tuple<string,string,string> parameterValue,string operatingLanguage,string batteryFeature)
     {
-        Console.WriteLine(displayMessage);
+        if (operatingLanguage == "English")
+        {
+            Display.PrintMessage(parameterValue.Item1);
+            if (parameterValue.Item3 == "Breach")
+            {
+                batteryAlerts.Add(batteryFeature, parameterValue.Item1);    
+            }
+        }
+        else
+        {
+            Display.PrintMessage(parameterValue.Item2);
+            if (parameterValue.Item3 == "Breach")
+            {
+                batteryAlerts.Add(batteryFeature, parameterValue.Item2);
+            }
+        }
     }
 
 }
