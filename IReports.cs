0fea35e569e927c4e096e81c2c6a424f3b7411d3
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-17 16:48:26 +0530

    Create IReports.cs

---
+++ IReports.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public interface IReports
+{
+    void DisplayBatteryReport(Dictionary<string, string> batteryAlerts);
+}
+
