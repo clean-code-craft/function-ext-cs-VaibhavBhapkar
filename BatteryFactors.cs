8aab1da424a39e12276f56bf5dd02bf0f0c6e208
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-17 16:46:21 +0530

    Update BatteryFactors.cs

--- BatteryFactors.cs
+++ BatteryFactors.cs
@@ -6,12 +6,13 @@ using System.Text;
 public class BatteryFactors
 {
     public float batteryTemprature, batteryStateOfCharge, batteryChargeRate;
-    public string operatingLanguage = string.Empty;
-    public BatteryFactors(float temprature, float soc, float chargeRate,string operatingLang)
+    public string operatingLanguage = string.Empty, temperatureMesureUnit=string.Empty;
+    public BatteryFactors(float temprature, float soc, float chargeRate, string operatingLang,string temperatureMesureUnit)
     {
         this.batteryTemprature = temprature;
         this.batteryStateOfCharge = soc;
         this.batteryChargeRate = chargeRate;
         this.operatingLanguage = operatingLang;
+        this.temperatureMesureUnit = temperatureMesureUnit;
     }
 }
