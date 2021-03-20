f9cd0d872799b2edefb7066c38db6fa8b9c65aac
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-20 18:39:12 +0530

    Update BatteryExamine.cs

--- BatteryExamine.cs
+++ BatteryExamine.cs
@@ -6,51 +6,55 @@ public class BatteryExamine
 {
     public bool BatteryIsOk(BatteryFactors batteryFactors)
     {
-        BatteryFactors.BatteryStatus temperatureStatus, socStatus, chargeRateStatus;
+        bool temperatureStatus, socStatus, chargeRateStatus;
         temperatureStatus=CompareTemperatureWithRange(batteryFactors.batteryTemprature);
         socStatus=CompareStateOfChargeWithRange(batteryFactors.batteryStateOfCharge);
         chargeRateStatus=CompareChargeRateWithRange(batteryFactors.batteryChargeRate);
-        if(temperatureStatus== BatteryFactors.BatteryStatus.Normal && socStatus == BatteryFactors.BatteryStatus.Normal &&chargeRateStatus == BatteryFactors.BatteryStatus.Normal)
-        {
-            return true;
-        }
-        return false;
+        return (temperatureStatus && socStatus && chargeRateStatus);
     }
-    private BatteryFactors.BatteryStatus CompareTemperatureWithRange(float batteryTemperature)
+    private bool CompareTemperatureWithRange(float batteryTemperature)
     {
         foreach (var temperatureValue in BatteryLimits.temperatureBoundary)
         {
             if (temperatureValue.Key.InRange(batteryTemperature))            
             {
                 Display.PrintMessage(temperatureValue.Value.Item1);
-                return temperatureValue.Value.Item2;
+                if(temperatureValue.Value.Item2!=BatteryFactors.BatteryStatus.Breach)
+                {
+                    return true;
+                }
             }
         }
-        return BatteryFactors.BatteryStatus.Breach;
+        return false;
     }
-    public BatteryFactors.BatteryStatus CompareStateOfChargeWithRange(float stateOfCharge)
+    public bool CompareStateOfChargeWithRange(float stateOfCharge)
     {
         foreach (var socValue in BatteryLimits.stateOfChargeBoundary)
         {
             if (socValue.Key.InRange(stateOfCharge))
             {
                 Display.PrintMessage(socValue.Value.Item1);
-                return socValue.Value.Item2;
+                if (socValue.Value.Item2 != BatteryFactors.BatteryStatus.Breach)
+                {
+                    return true;
+                }
             }
         }
-        return BatteryFactors.BatteryStatus.Breach;
+        return false;
     }
-    public BatteryFactors.BatteryStatus CompareChargeRateWithRange(float chargeRate)
+    public bool CompareChargeRateWithRange(float chargeRate)
     {
         foreach (var chargeRateValue in BatteryLimits.chargeRateBoundary)
         {
             if (chargeRateValue.Key.InRange(chargeRate))
             {
                 Display.PrintMessage(chargeRateValue.Value.Item1);
-                return chargeRateValue.Value.Item2;
+                if (chargeRateValue.Value.Item2 != BatteryFactors.BatteryStatus.Breach)
+                {
+                    return true;
+                }
             }
         }
-        return BatteryFactors.BatteryStatus.Breach;
+        return false;
     }  
 }
-
