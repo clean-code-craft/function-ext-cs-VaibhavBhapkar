1b803600e8544af12c615810ea9d314608649cae
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-20 18:40:46 +0530

    Update BatteryFactors.cs

--- BatteryFactors.cs
+++ BatteryFactors.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
-
 public class BatteryFactors
 {
     public float batteryTemprature, batteryStateOfCharge, batteryChargeRate;
-    public string operatingLanguage = string.Empty, temperatureMesureUnit=string.Empty;
-    public BatteryFactors(float temprature, float soc, float chargeRate, string operatingLang,string temperatureMesureUnit)
+    public BatteryFactors(float temprature, float soc, float chargeRate)
     {
         this.batteryTemprature = temprature;
         this.batteryStateOfCharge = soc;
         this.batteryChargeRate = chargeRate;
-        this.operatingLanguage = operatingLang;
-        this.temperatureMesureUnit = temperatureMesureUnit;
+    }
+    public enum BatteryStatus
+    {
+        Breach,
+        Warning,
+        Normal
     }
 }
