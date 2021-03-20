f47a29e0e1e52689755700d78f36c6d1d558db11
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-20 18:41:46 +0530

    Update BatteryLimits.cs

--- BatteryLimits.cs
+++ BatteryLimits.cs
@@ -4,36 +4,36 @@ using System.Text;
 
 public static class BatteryLimits
 {
-    private static IReadOnlyDictionary<Interval, Tuple<string, string,string>> _stateOfChargeBoundary
-    = new Dictionary<Interval, Tuple<string, string,string>>
+    
+    private static IReadOnlyDictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>> _stateOfChargeBoundary
+    = new Dictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>>
        {
-           { new Interval(0, 20), new Tuple<string,string,string>("The state of charge is low compared to the minimum limits","Der Ladezustand ist im Vergleich zu den Mindestgrenzen niedrig","Breach") },
-           { new Interval(21, 24), new Tuple<string,string,string>("Warning! State of charge is heading towards minimum limit value","Warnung! Der Ladezustand nähert sich dem Mindestgrenzwert","Normal") },
-           { new Interval(25, 75), new Tuple<string,string,string>("State of Charge is Normal","Der Ladezustand ist normal","Normal")},
-           { new Interval(76, 80), new Tuple<string,string,string>("Warning! State of charge is heading towards maximum limit value","Warnung! Der Ladezustand nähert sich dem maximalen Grenzwert","Normal") },
-           { new Interval(81, 100), new Tuple<string,string,string>("The state of charge is high compared to the maximum limits","Der Ladezustand ist im Vergleich zu den Höchstgrenzen hoch","Breach") }
+           { new Interval(0, 20), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Low_Soc"),BatteryFactors.BatteryStatus.Breach) },
+           { new Interval(21, 24), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Warning_Soc_Low"),BatteryFactors.BatteryStatus.Warning) },
+           { new Interval(25, 75), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Normal_Soc"),BatteryFactors.BatteryStatus.Normal)},
+           { new Interval(76, 80), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Warning_Soc_High"),BatteryFactors.BatteryStatus.Warning) },
+           { new Interval(81, 100), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("High_Soc"),BatteryFactors.BatteryStatus.Breach) }
         };
-    public static IReadOnlyDictionary<Interval, Tuple<string, string,string>> stateOfChargeBoundary => _stateOfChargeBoundary;
+    public static IReadOnlyDictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>> stateOfChargeBoundary => _stateOfChargeBoundary;
 
-    private static IReadOnlyDictionary<Interval, Tuple<string, string, string>> _temperatureBoundary
-    = new Dictionary<Interval, Tuple<string, string, string>>
+    private static IReadOnlyDictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>> _temperatureBoundary
+    = new Dictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>>
        {
-           { new Interval(-45, -0.01f), new Tuple<string,string,string>("The temperature is low compared to the minimum limits","Die Temperatur ist im Vergleich zu den Mindestgrenzwerten niedrig","Breach") },
-           { new Interval(0, 2.25f), new Tuple<string,string,string>("Warning! Temperature is heading towards minimum limit value","Warnung! Die Temperatur nähert sich dem Mindestgrenzwert","Normal") },
-           { new Interval(2.26f, 42.75f), new Tuple<string,string,string>("Temperature is Normal","Die Temperatur ist normal","Normal")},
-           { new Interval(42.76f, 45), new Tuple<string,string,string>("Warning! Temperature is heading towards maximum limit value","Warnung! Die Temperatur nähert sich dem maximalen Grenzwert","Normal") },
-           { new Interval(46, 100), new Tuple<string,string,string>("The temperature is high compared to the maximum limits","Die Temperatur ist im Vergleich zu den Höchstgrenzen hoch","Breach") }
+           { new Interval(-45, -0.01f), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Low_Temperature"),BatteryFactors.BatteryStatus.Breach) },
+           { new Interval(0, 2.25f), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Warning_Temperature_Low"),BatteryFactors.BatteryStatus.Warning) },
+           { new Interval(2.26f, 42.75f), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Normal_Temperature"),BatteryFactors.BatteryStatus.Normal)},
+           { new Interval(42.76f, 45), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Warning_Temperature_High"),BatteryFactors.BatteryStatus.Warning) },
+           { new Interval(46, 100), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("High_Temperature"),BatteryFactors.BatteryStatus.Breach) }
         };
-    public static IReadOnlyDictionary<Interval, Tuple<string, string, string>> temperatureBoundary => _temperatureBoundary;
+    public static IReadOnlyDictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>> temperatureBoundary => _temperatureBoundary;
 
-    private static IReadOnlyDictionary<Interval, Tuple<string, string, string>> _chargeRateBoundary
-   = new Dictionary<Interval, Tuple<string, string,string>>
+    private static IReadOnlyDictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>> _chargeRateBoundary
+   = new Dictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>>
       {
-           { new Interval(0, 0.75f), new Tuple<string,string, string>("Charge Rate is Normal","Laderate ist normal","Normal")},
-           { new Interval(0.76f, 0.8f), new Tuple<string,string, string>("Warning! Charge rate is heading towards maximum limit value","Warnung! Der Gebührensatz bewegt sich in Richtung des maximalen Grenzwerts","Normal") },
-           { new Interval(0.81f, 100), new Tuple<string,string, string>("The charge rate is high compared to the maximum limits","Die Laderate ist im Vergleich zu den Höchstgrenzen hoch","Breach") }
+           { new Interval(0, 0.75f), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Normal_ChargeRate"),BatteryFactors.BatteryStatus.Normal)},
+           { new Interval(0.76f, 0.8f), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("Warning_ChargeRate_High"),BatteryFactors.BatteryStatus.Warning) },
+           { new Interval(0.81f, 100), new Tuple<string,BatteryFactors.BatteryStatus>(CultureInformation.GetCultureInformation().GetString("High_ChargeRate"),BatteryFactors.BatteryStatus.Breach) }
        };
-    public static IReadOnlyDictionary<Interval, Tuple<string, string, string>> chargeRateBoundary => _chargeRateBoundary;
+    public static IReadOnlyDictionary<Interval, Tuple<string, BatteryFactors.BatteryStatus>> chargeRateBoundary => _chargeRateBoundary;
 
 }
-
