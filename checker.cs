dc96314897f5abfe7a94579bb28831e67e7a470f
clean-code-craft <68837213+clean-code-craft@users.noreply.github.com>
2021-03-21 12:12:40 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -10,6 +10,16 @@ class Checker
         
          
             System.Console.WriteLine("hello");
+         Assembly assem =System.Reflection.Assembly.GetExecutingAssembly();
+      Console.WriteLine("File: {0}", filename);
+         
+      // Enumerate the resource files.
+      string[] resNames = assem.GetManifestResourceNames();
+      if (resNames.Length == 0)
+         Console.WriteLine("   No resources found.");
+
+      foreach (var resName in resNames)
+         Console.WriteLine("   Resource: {0}", resName.Replace(".resources", ""));
         
         
     }
