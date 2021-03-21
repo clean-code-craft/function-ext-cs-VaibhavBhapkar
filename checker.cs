135b8e61494a722ca70ff31e6dad8df56bfa2059
clean-code-craft <cleancodcraft@gmail.com>
2021-03-21 12:03:57 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -9,87 +9,9 @@ class Checker
     {
         
          
-    
-        string value =en_US.TemperatureUnit;
+            System.Console.WriteLine("hello");
         
-        /*
-        float temperatureInput, stateOfChargeInput, chargeRateInput;
-        ResourceManager resourceInfo = GetLanguageInput();
-        CultureInformation.SetCultureInformation(resourceInfo);
-        temperatureInput = GetTemperatureUnit();
-        stateOfChargeInput = GetStateOfChargeInput();
-        chargeRateInput = GetChargeRateInput();
-        BatteryExamine batteryExamine = new BatteryExamine();
-        bool result = batteryExamine.BatteryIsOk(new BatteryFactors(temperatureInput, stateOfChargeInput, chargeRateInput));
-        BatteryParameterBreachSubject batteryParameterBreachSubject = new BatteryParameterBreachSubject();
-        AccumulateBreachParameter accumulateBreachParameter = new AccumulateBreachParameter();
-        batteryParameterBreachSubject.Attach(accumulateBreachParameter);
-        batteryParameterBreachSubject.BatteryFeaturesBreachCheck(new BatteryFactors(temperatureInput, stateOfChargeInput, chargeRateInput));
-        Dictionary<string, string> batteryAlertMessages = accumulateBreachParameter.GetReport();
-        IReports iReportsCosoleLogger = new ConsoleReportLogger();
-        BatteryReport batteryReportWithConsoleLogger = new BatteryReport(iReportsCosoleLogger);
-        batteryReportWithConsoleLogger.ReportLogger(batteryAlertMessages);
-        IReports iReportsDummyLogger = new DummyReportLogger();
-        BatteryReport batteryReportWithDummyLogger = new BatteryReport(iReportsDummyLogger);
-        batteryReportWithDummyLogger.ReportLogger(batteryAlertMessages);
-        */
-
+        
     }
-    static ResourceManager GetLanguageInput()
-    {
-        Display.PrintMessage("Please select language 1.English 2.German");
-        int languageChoice = Convert.ToInt32(Console.ReadLine());
-       // if (languageChoice == 1)
-        //{
-            ICulterSet culterSet = new EnglishCulterSetting();
-            CulterHelper culterHelper = new CulterHelper(culterSet);
-            return culterHelper.SetCulter();
-        //}
-        //else
-       // {
-          //  ICulterSet culterSet = new GermanCulterSetting();
-            //CulterHelper culterHelper = new CulterHelper(culterSet);
-            //return culterHelper.SetCulter();
-        //}
-    }
-    static float GetTemperatureUnit()
-    {
-        string message = string.Empty;
-        int temperatureUnit;
-        message = CultureInformation.GetCultureInformation().GetString("TemperatureUnit");
-        Display.PrintMessage(message);
-        temperatureUnit = Convert.ToInt32(Console.ReadLine());
-        return GetTemperatureValue(temperatureUnit);
-    }
-    static float GetTemperatureValue(int tempUnit)
-    {
-        string message = string.Empty;
-        message = CultureInformation.GetCultureInformation().GetString("TemperatureValue");
-        Display.PrintMessage(message);
-        if (tempUnit == 1)
-        {
-            return float.Parse(Console.ReadLine());
-        }
-        else
-        {
-            return BatteryFeatureConversion.ConvertTemperatureFahrenheitToCelsius(float.Parse(Console.ReadLine()));
-        }
-    }
-    static float GetStateOfChargeInput()
-    {
-        string message = string.Empty;
-        message = CultureInformation.GetCultureInformation().GetString("StateOfChargeValue");
-        Display.PrintMessage(message);
-        return float.Parse(Console.ReadLine());
-    }
-    static float GetChargeRateInput()
-    {
-        string message = string.Empty;
-        message = CultureInformation.GetCultureInformation().GetString("ChargeRateValue");
-        Display.PrintMessage(message);
-        return float.Parse(Console.ReadLine());
-
-    }
-
 }
 
